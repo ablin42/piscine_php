fbf0669b921ce592612b30cc05941a93ca2db1a0
ablin <ablin@student.42.fr>
2018-10-02 02:50:22 +0200

    saving, getting back at it tomorrow

---
+++ Day01/ex09/ssap2.php
@@ -0,0 +1,18 @@
+#!/usr/bin/php
+<?php
+$str = "";
+if ($argc > 1)
+{
+	for ($i = 1; $i < $argc; $i++)
+	{
+		$str .= " {$argv[$i]} ";
+	}
+	$arr = preg_split('/ +/', trim($str));
+	sort($arr);
+	$nb = count($arr);
+	for ($j = 0; $j < $nb; $j++)
+	{
+		echo "{$arr[$j]}\n";
+	}
+}
+?>
