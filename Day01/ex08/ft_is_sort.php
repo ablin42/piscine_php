fe90870a779f1ebddfdf7e406ded996d0a14cd28
ablin <ablin@student.42.fr>
2018-10-02 02:44:59 +0200

    ex07 and 08 done

---
+++ Day01/ex08/ft_is_sort.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?php
+function	ft_is_sort($tab)
+{
+	$nb = count($tab);
+	print_r($tab);
+	for ($i = 0; $i < $nb - 1; $i++)
+	{
+		echo "{$tab[$i]} {$tab[$i + 1]}\n";
+		if ($tab[$i] > $tab[$i + 1])
+			return (false);
+	}
+	return (true);
+}
+?>
