baf095455a3e30b4abd14137ae372659bb07bd54
ablin <30326647+ablin42@users.noreply.github.com>
2018-10-06 02:40:14 +0200

    Update login.php

--- Day04/ex04/login.php
+++ Day04/ex04/login.php
@@ -4,9 +4,18 @@ include ("auth.php");
 if (isset($_POST['login']) && isset($_POST['passwd']))
 {
 	if (auth($_POST['login'], $_POST['passwd']) === true)
-	{
-		echo "<iframe height=\"550px\" src=\"chat.php\"></iframe>";
-		echo "<iframe height=\"55px\" src=\"speak.php\"></iframe>";
+	{?>
+		<!DOCTYPE html>
+       		<html>
+        	<head>
+            		<meta charset="UTF-8">
+            		<title>42Chat</title>
+        	</head>
+        	<body>
+           		 <iframe name="chat" src="chat.php" width="100%" height="550px"></iframe>
+            		<iframe name="speak" src="speak.php" width="100%" height="50px"></iframe>
+        	</body>
+		</html>
 	}
 	else
 	{
