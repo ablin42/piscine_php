970ca7fda76acc261e8bb83aa2448714bcba5d8e
ablin <ablin@student.42.fr>
2018-10-09 16:32:50 +0200

    saving progress

--- Day05/ex11/ex11.sql
+++ Day05/ex11/ex11.sql
@@ -1,6 +1,6 @@
 SELECT UPPER(`last_name`) AS `NAME`, `first_name`, `price`
 FROM `member`
 INNER JOIN `subscription` ON member.id_sub = subscription.id_sub
-INNER JOIN `user_card` ON member.id_member = user_card.id_user
+INNER JOIN `user_card` ON member.id_member = user_card.id_user //id_user_card
 WHERE subscription.price > '42'
 ORDER BY `last_name` ASC, `first_name` ASC;
