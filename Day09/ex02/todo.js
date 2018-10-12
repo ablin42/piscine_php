22cd6f74a3023b9c6b00f8850a48a98be9710083
ablin <ablin@student.42.fr>
2018-10-13 01:47:19 +0200

    saving progress for Day09

---
+++ Day09/ex02/todo.js
@@ -0,0 +1,22 @@
+function createDiv() {
+    var content = prompt("Your to-do", "Ex: What you won't do");
+    var nbelem = document.getElementById("ft_list").childElementCount;
+
+    if (content != null && content != "")
+    {
+        var div = document.createElement("div");
+        div.style.height = "40px";
+        div.style.minWidth = "100px";
+        div.style.padding = "25px 10px 0px 10px";
+        div.style.margin = "5px";
+        div.style.background = "#4e4e4e";
+        div.style.border = "2px solid orange";
+        div.style.color = "white";
+        div.className = "todo_" + nbelem;
+        div.innerHTML = content;
+        div.onclick = function(){
+            this.parentElement.removeChild(this);
+    };
+        document.getElementById("ft_list").appendChild(div);
+    }
+}
